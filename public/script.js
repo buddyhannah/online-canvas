d11ae8be8f2727f90d2aea4440a314544386d545
j9631 <jacob901@nmsu.edu>
2025-04-19 18:32:43 -0600

    Added style to login and pan + zoom to canvas

--- public/script.js
+++ public/script.js
@@ -1,14 +1,14 @@
-const canvas = new fabric.Canvas('c');
+const canvas = new fabric.Canvas('c', {fireMiddleClick: true});
+let zoomLevel = 1;
+
 canvas.setHeight(window.innerHeight);
 canvas.setWidth(window.innerWidth);
-
 canvas.isDrawingMode = true;
 
 const drawBtn = document.getElementById('draw');
 const eraseBtn = document.getElementById('erase');
 const colorPicker = document.getElementById('colorPicker');
 const brushSize = document.getElementById('brushSize');
-
 
 // Drawing brush
 drawBtn.addEventListener('click', () => {
@@ -64,6 +64,71 @@ function parseJWT(token){
 
   return JSON.parse(jsonPayload);
 }
+
+// Zoom in/out logic
+document.getElementById('zoomIn').onclick = () => {
+  zoomLevel = Math.min(zoomLevel + 0.1, 3); // max 300%
+  canvas.setZoom(zoomLevel);
+  centerViewport();
+};
+
+document.getElementById('zoomOut').onclick = () => {
+  zoomLevel = Math.max(zoomLevel - 0.1, 0.2); // min 20%
+  canvas.setZoom(zoomLevel);
+  centerViewport();
+};
+
+// Center canvas on load
+window.addEventListener('load', () => {
+  setTimeout(centerViewport, 100); // slight delay to ensure layout
+});
+
+// Center canvas when zoomed
+function centerViewport() {
+  const wrapper = document.querySelector('.canvas-wrapper');
+  wrapper.scrollLeft = (canvas.getWidth() * zoomLevel - wrapper.clientWidth) / 2;
+  wrapper.scrollTop = (canvas.getHeight() * zoomLevel - wrapper.clientHeight) / 2;
+}
+
+
+// Samples for scroll and pan taken from here: https://fabricjs.com/docs/old-docs/fabric-intro-part-5/ with minor tweaks
+canvas.on('mouse:wheel', function(opt) {
+  var delta = opt.e.deltaY;
+  var zoom = canvas.getZoom();
+  zoom *= 0.999 ** delta;
+  if (zoom > 3) zoom = 3;
+  if (zoom < 0.2) zoom = 0.2;
+  canvas.setZoom(zoom);
+  opt.e.preventDefault();
+  opt.e.stopPropagation();
+})
+canvas.on('mouse:down', function(opt) {
+  var evt = opt.e;
+  console.log(evt.button);
+  if (evt.button == 1) {
+    this.isDragging = true;
+    this.selection = false;
+    this.lastPosX = evt.clientX;
+    this.lastPosY = evt.clientY;
+  }
+});
+
+canvas.on('mouse:move', function(opt) {
+  if (this.isDragging) {
+    var e = opt.e;
+    var vpt = this.viewportTransform;
+    vpt[4] += e.clientX - this.lastPosX;
+    vpt[5] += e.clientY - this.lastPosY;
+    this.requestRenderAll();
+    this.lastPosX = e.clientX;
+    this.lastPosY = e.clientY;
+  }
+});
+canvas.on('mouse:up', function(opt) {
+  this.setViewportTransform(this.viewportTransform);
+  this.isDragging = false;
+  this.selection = true;
+});
 
 const token = localStorage.getItem('token');
 console.log(token)
